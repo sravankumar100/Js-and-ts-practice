f9c10e7c8746afb9d778e506bec12cbc0e285f1c
Sravankumar <badesravankumar1000@gmail.com>
2026-04-19 21:58:50 +0530

    message

---
+++ typescripttraining/conditionalstatements-program.ts
@@ -0,0 +1,14 @@
+let marks:number=95;
+if(marks >90){
+    console.log("pass in 1 stage");
+
+if(marks <95){
+console.log("Need Qualification for stage2");
+}
+else if(marks >=95){
+console.log("pass in 2 stage");
+}
+else{
+    console.log("fail in 2 stage");
+}
+}
